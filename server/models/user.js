2f4efae86614708aa183f9ba5ad18552871b59ca
Victor Chang <victorchang91@gmail.com>
2016-03-06 00:24:23 -0800

    Test github account switch

--- server/models/user.js
+++ server/models/user.js
@@ -6,7 +6,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   username: { type: 'String', required: true },
   pirate_lvl: { type: 'Number' },
-  user_id: { type: 'Number', required: true},
+  user_id: { type: 'Number', required: true },
 });
 
 
