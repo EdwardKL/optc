2500c730b4d060ad1ff2df70450f876436657425
Victor Chang <victorchang91@gmail.com>
2016-03-07 22:42:38 -0800

    Added Captain model, updated User model

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,19 @@
 // User model
 
+import Captain from './captain';
 import mongoose from 'mongoose';
+
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+  _id: { type: Schema.Types.ObjectId },
   username: { type: 'String', required: true },
+  password: { type: 'String', required: true },
   pirate_lvl: { type: 'Number' },
   user_id: { type: 'Number', required: true },
+  captains: [Captain],
+  date_created: { type: 'Date', default: Date.now },
+  last_login: { type: 'Date' }
 });
-
 
 export default mongoose.model('User', userSchema);
