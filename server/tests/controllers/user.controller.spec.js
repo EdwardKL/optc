ca68d055003f0985cf10035b4af7dcf67c455c1a
EdwardKL <edward.k.lu@gmail.com>
2016-04-28 00:51:06 -0700

    Minor auto-formats for eslint.

--- server/tests/controllers/user.controller.spec.js
+++ server/tests/controllers/user.controller.spec.js
@@ -11,7 +11,7 @@ import mongoConfig from '../../config';
 const expect = chai.expect;
 
 function connectDB(done) {
-  mongoose.connect(mongoConfig.mongoURL, function (err) {
+  mongoose.connect(mongoConfig.mongoURL, function(err) {
     if (err) return done(err);
     done();
   });
@@ -22,32 +22,38 @@ function dropDB(done) {
     return done();
   }
 
-  mongoose.connection.db.dropDatabase(function (err) {
-    if (err) throw err;
+  mongoose.connection.db.dropDatabase(function(err) {
+    if (err)
+      throw err;
     mongoose.connection.close(done);
   });
 }
 
-describe('POST /signup', function () {
+describe('POST /signup', function() {
 
-  beforeEach('connect to db', function (done) {
-    connectDB(function () {
+  beforeEach('connect to db', function(done) {
+    this.timeout(3 * 1e3);
+    connectDB(function() {
       done();
     });
   });
 
-  afterEach(function (done) {
+  afterEach(function(done) {
     dropDB(done);
   });
 
-  it('Should correctly register a user', function (done) {
+  it('Should correctly register a user', function(done) {
     // 5s timeout. Eventually we'll want to keep the default of 2s, but our site is slow :(
     this.timeout(5 * 1e3);
     request(app)
       .post('/signup')
-      .send({ username: 'test-user', password: 'asdf', password_confirmation: 'asdf' })
-      .end(function (err, res) {
-        User.findByUsername('test-user', function (err, user) {
+      .send({
+        username: 'test-user',
+        password: 'asdf',
+        password_confirmation: 'asdf'
+      })
+      .end(function(err, res) {
+        User.findByUsername('test-user', function(err, user) {
           expect(user.username).to.equal('test-user');
           done();
         });
--- shared/components/Account/Account.jsx
+++ shared/components/Account/Account.jsx
@@ -1,21 +1,21 @@
-import React, {Component, PropTypes} from 'react';
-import {Panel, Row, Col, Button} from 'react-bootstrap';
+import React, { Component, PropTypes } from 'react';
+import { Panel, Row, Col, Button } from 'react-bootstrap';
 import AccountEditor from '../../components/Account/AccountEditor';
 import CaptainEditor from '../../components/Captain/CaptainEditor';
 import Captain from '../../components/Captain/Captain';
 
 // TODO: Maybe move this to a library.
 String.prototype.capitalizeFirstLetter = function() {
-    return this.charAt(0).toUpperCase() + this.slice(1);
+  return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
 class Account extends Component {
-  constructor(props, context){
+  constructor(props, context) {
     super(props, context);
     this.state = {};
     this.state.account_data = props.account_data;
   }
-  
+
   render() {
     return (
       <Panel key={this.state.account_data.id} className="accountPanel">
@@ -29,21 +29,21 @@ class Account extends Component {
         </Col>
         <Col xs={9}>
           {this.state.account_data._captains.map(function(captain) {
-            return <Captain captain_data={captain} account_id={this.state.account_data.id} key={captain._id}/>
-          }.bind(this))}
+        return <Captain captain_data={captain} account_id={this.state.account_data.id} key={captain._id}/>
+      }.bind(this))}
         </Col>
         <Col xs={1}>
           <CaptainEditor edit={false} account_id={this.state.account_data.id}><span>Add Captain</span></CaptainEditor>
           <Row>
             <AccountEditor
-              edit={true}
-              crew_name={this.state.account_data.crew_name}
-              friend_id={this.state.account_data.friend_id}
-              pirate_level={this.state.account_data.pirate_level}
-              region={this.state.account_data.region}
-              account_id={this.state.account_data.id}/>
+      edit={true}
+      crew_name={this.state.account_data.crew_name}
+      friend_id={this.state.account_data.friend_id}
+      pirate_level={this.state.account_data.pirate_level}
+      region={this.state.account_data.region}
+      account_id={this.state.account_data.id}/>
           </Row>
-          <Row><a href={"/accounts/delete/id".replace("id",this.state.account_data.id)}>Delete</a></Row>
+          <Row><a href={"/accounts/delete/id".replace("id", this.state.account_data.id)}>Delete</a></Row>
         </Col>
       </Panel>
     )
