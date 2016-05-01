e84aec6a77f416fa31df9b3143d18ffa37f3efb2
EdwardKL <edward.k.lu@gmail.com>
2016-05-01 12:50:11 -0700

    use exports instead of export. istanbul doesn't like the latter as much.

--- server/controllers/utils.js
+++ server/controllers/utils.js
@@ -2,35 +2,35 @@ import CaptainModel from '../models/captain';
 
 // Redirects to /signup if the user is not logged in.
 // Returns true if redirected.
-export function redirectIfLoggedOut(req, res, next) {
+exports.redirectIfLoggedOut = function redirectIfLoggedOut(req, res, next) {
   // Don't call next, because we shouldn't redirect.
   if (req.user) return false;
   req.flash('error_message', 'Please sign in.');
   res.redirect('/signup');
   next();
   return true;
-}
+};
 
 // Converts to a number. If input is undefined, this will return 0.
-export function getNumber(num) {
+exports.getNumber = function getNumber(num) {
   return num ? Number(num) : 0;
-}
+};
 
 // Array should be an array of object ids, and id should be either an object id
 // or its string representation.
 // Returns true if array has id.
-export function hasId(array, id) {
+exports.hasId = function hasId(array, id) {
   for (const item of array) {
     if (item.toString().valueOf() === id.toString().valueOf()) {
       return true;
     }
   }
   return false;
-}
+};
 
 // Recursively removes the captains referred to by id by the given captains array.
 // Calls cb when finished.
-export function removeCaptains(captains, cb) {
+exports.removeCaptains = function removeCaptains(captains, cb) {
   if (captains.length === 0) {
     cb();
     return;
@@ -40,4 +40,4 @@ export function removeCaptains(captains, cb) {
     if (err) throw err;
     removeCaptains(captains, cb);
   });
-}
+};
