285c0034033d63e0e381f778b50cc97136d58fd9
EdwardKL <edward.k.lu@gmail.com>
2016-05-11 22:43:06 -0700

    Add a home page.

--- shared/container/Home/HomeView.jsx
+++ shared/container/Home/HomeView.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import {Grid} from 'react-bootstrap';
+import { Grid, Jumbotron, PageHeader } from 'react-bootstrap';
 
 class Home extends React.Component {
-  constructor(props, context){
+  constructor(props, context) {
     super(props, context);
   }
 
   render() {
-    return(<Grid id="content" />);
+    return (
+      <Grid id="content">
+        <PageHeader>Ohara <small>One Piece Treasure Cruise's Library</small></PageHeader>
+        <Jumbotron id="newsbox">
+          <h3>News</h3>
+          <p>Alpha launch by EOQ.</p>
+          <p>Alpha friend finder online.</p>
+        </Jumbotron>
+      </Grid>);
   }
 }
 
