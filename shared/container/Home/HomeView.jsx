681b56ec5083d687cc11490c4c067c0a2ce5f736
EdwardKL <edward.k.lu@gmail.com>
2017-01-22 19:11:44 -0800

    Remove obnoxious home ad

--- shared/container/Home/HomeView.jsx
+++ shared/container/Home/HomeView.jsx
@@ -16,41 +16,29 @@ class Home extends React.Component {
         <PageHeader>Ohara <small>One Piece Treasure Cruise&rsquo;s Library</small></PageHeader>
         <div id="homeContent">
           <Row>
-            <Col xs={7}>
-              <a href="/signup">
-                <div className="homeEntry">
-                  <span className="entryHeader">Sign Up</span>
-                  <br />
-                  <span className="entryBlurb">Add your OPTC account so others can find you, and post on our site!</span>
-                </div>
-              </a>
-              <br />
-              <a href="/friend_finder">
-                <div className="homeEntry homeFF">
-                  <span className="entryHeader">Friend Finder</span>
-                  <br />
-                  <span className="entryBlurb">Find players with the captains you need!</span>
-                </div>
-              </a>
-              <br />
-              <a href="/units">
-                <div className="homeEntry homeUnits">
-                  <span className="entryHeader">Units</span>
-                  <br />
-                  <span className="entryBlurb">Lookup unit data, posts and recommendations!</span>
-                </div>
-              </a>
-            </Col>
-            <Col xs={5}>
-              <div className="ad">
-                <ins
-                  className="adsbygoogle homeAd"
-                  data-ad-client="ca-pub-3382549750623853"
-                  data-ad-slot="1624873575"
-                >
-                </ins>
+            <a href="/signup">
+              <div className="homeEntry">
+                <span className="entryHeader">Sign Up</span>
+                <br />
+                <span className="entryBlurb">Add your OPTC account so others can find you, and post on our site!</span>
               </div>
-            </Col>
+            </a>
+            <br />
+            <a href="/friend_finder">
+              <div className="homeEntry homeFF">
+                <span className="entryHeader">Friend Finder</span>
+                <br />
+                <span className="entryBlurb">Find players with the captains you need!</span>
+              </div>
+            </a>
+            <br />
+            <a href="/units">
+              <div className="homeEntry homeUnits">
+                <span className="entryHeader">Units</span>
+                <br />
+                <span className="entryBlurb">Lookup unit data, posts and recommendations!</span>
+              </div>
+            </a>
           </Row>
           <hr />
           <div id="news">
